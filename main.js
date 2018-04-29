eb96711f42dcf2125b3e10385033333bfb4791ff
niranjanthiru <niranjanthirusangu@gmail.com>
2018-04-29 12:59:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,62 @@
+"use strict";
+var WebSocketServer = require('websocket').server;
+var http = require('http');
+
+var server = http.createServer(function (request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(404);
+    response.end();
+});
+server.listen(8080, function () {
+    console.log((new Date()) + ' Server is listening on port 8080');
+});
+
+var wsServer = new WebSocketServer({
+    httpServer: server,
+    // You should not use autoAcceptConnections for production
+    // applications, as it defeats all standard cross-origin protection
+    // facilities built into the protocol and the browser.  You should
+    // *always* verify the connection's origin and decide whether or not
+    // to accept it.
+    autoAcceptConnections: false
+});
+
+function originIsAllowed(origin) {
+    // put logic here to detect whether the specified origin is allowed.
+    return true;
+}
+var clients = {};
+wsServer.on('request', function (request) {
+    if (!originIsAllowed(request.origin)) {
+        // Make sure we only accept requests from an allowed origin
+        request.reject();
+        console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+        return;
+    }
+
+    var connection = request.accept('echo-protocol', request.origin);
+    console.log((new Date()) + ' Connection accepted.');
+    var UserID = 0;
+    connection.on('message', function (message) {
+        if (message.type === 'utf8') {
+            var messageObject = JSON.parse(message.utf8Data);
+            if (messageObject.UserID) {
+                UserID = messageObject.UserID;
+                clients[UserID] = connection;
+                if (messageObject.hasOwnProperty("ToUserID") && clients.hasOwnProperty(messageObject.ToUserID)) {
+                    clients[messageObject.ToUserID].sendUTF(message.utf8Data);
+                }
+            }
+            console.log('Received Message: ' + message.utf8Data);
+            // connection.sendUTF(message.utf8Data);
+        }
+        else if (message.type === 'binary') {
+            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+            connection.sendBytes(message.binaryData);
+        }
+    });
+    connection.on('close', function (reasonCode, description) {
+        delete clients[UserID];
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    });
+});
